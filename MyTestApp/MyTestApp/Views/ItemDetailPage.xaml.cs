4e23ac21c40e9b9a88af2123cc81419b4c3eaf7f
James Hurley <jameshurley@ISTs-iMac-3.lan>
2022-09-09 17:23:43 -0400

    initial commit

---
+++ MyTestApp/MyTestApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using MyTestApp.ViewModels;
+
+namespace MyTestApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ MyTestApp/MyTestApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using MyTestApp.Models;
+using MyTestApp.ViewModels;
+
+namespace MyTestApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
