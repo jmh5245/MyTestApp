ed18ce7b4df76c3041199e6239be393b3c34785a
James Hurley <jameshurley@ISTs-iMac-3.lan>
2022-09-09 17:36:26 -0400

    test commit

--- MyTestApp/MyTestApp/Models/Item.cs
+++ MyTestApp/MyTestApp/Models/Item.cs
@@ -7,5 +7,6 @@ namespace MyTestApp.Models
         public string Id { get; set; }
         public string Text { get; set; }
         public string Description { get; set; }
+        public int price { get; set; }
     }
 }
